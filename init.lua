bf5523af3828a59ea27ae3200fac19836ddce02a
choiqz <choiqz@bc.edu>
2023-04-25 12:42:47 -0400

    added repository containing nvim config

---
+++ init.lua
@@ -0,0 +1,14 @@
+require('settings')
+require('plugins')
+require('colors.gruvbox-material')
+require('nvim-tree-config')
+require('mappings')
+require('telescope-config')
+require('treesitter-config')
+require('neoscroll-config')
+require('lsp-config')
+require('nvim-autopairs-config')
+
+
+
+ 
---
+++ lua/settings/init.lua
@@ -0,0 +1,30 @@
+local set = vim.opt
+
+set.expandtab = true
+set.smarttab = true
+set.shiftwidth = 4 
+set.tabstop = 4
+set.softtabstop = 4
+set.expandtab = false
+
+set.hlsearch = false 
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
+
+set.splitbelow = true
+set.splitright = true
+set.wrap = true 
+set.breakindent = true
+set.scrolloff = 8 
+set.fileencoding = 'utf-8'
+set.termguicolors = true
+
+set.relativenumber = true
+set.cursorline = true
+
+set.hidden = true
+
+set.number = true
+
+
